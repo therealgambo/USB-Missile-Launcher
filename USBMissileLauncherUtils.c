c81d382d522fb2ac75e3bf5a30979badfc5c3e1f
Kris <missionrulz@hotmail.com>
2015-05-13 00:59:51 +1200

    IP Log Information
    Add IP information for remote commands

--- USBMissileLauncherUtils.c
+++ USBMissileLauncherUtils.c
@@ -58,22 +58,22 @@ int HandleKeyboardEvent(struct input_event *ev, int events, int device_type);
 void usage(char *filename) {
   fprintf(stderr, "\n"
     "=================================================================\n"
-	  "=       M&S USB Missile Launcher - Created by Luke Cole         =\n"
-	  "=          Network Support added by Kris Gambirazzi             =\n"
-	  "=================================================================\n"
-	  "   Usage: %s [option]\n\n"
+    "=       M&S USB Missile Launcher - Created by Luke Cole         =\n"
+    "=          Network Support added by Kris Gambirazzi             =\n"
+    "=================================================================\n"
+    "   Usage: %s [option]\n\n"
 
-	  "   Options:\n"
-	  "   -h            Help\n"
-	  "   -n            TCP Server. Port: 50505\n"
-	  "   -F            Fire Missile\n"
-	  "   -L            Rotate Left\n"
-	  "   -R            Rotate Right\n"
-	  "   -U            Tilt Up\n"
-	  "   -D            Tilt Down\n"
-	  "   -S n          Stop after n milliseconds (> 100)\n",
-	  
-	  filename);
+    "   Options:\n"
+    "   -h            Help\n"
+    "   -n            TCP Server. Port: 50505\n"
+    "   -F            Fire Missile\n"
+    "   -L            Rotate Left\n"
+    "   -R            Rotate Right\n"
+    "   -U            Tilt Up\n"
+    "   -D            Tilt Down\n"
+    "   -S n          Stop after n milliseconds (> 100)\n",
+    
+    filename);
 }
 
 //=============================================================================
@@ -107,34 +107,34 @@ void UDPServer(){
       len = sizeof(cliaddr);
       recvfrom(sockfd,buf,512,0,(struct sockaddr *)&cliaddr, &len);
 
-	  fprintf(stderr, "Received command %s from %s\n", buf, inet_ntoa(cliaddr.sin_addr));         
-		
-		if (strncmp(buf,left,1) == 0)
-		  msg = MISSILE_LAUNCHER_CMD_LEFT;
+    fprintf(stderr, "Received command %s from %s\n", buf, inet_ntoa(cliaddr.sin_addr));         
     
-		else if (strncmp(buf,right,1) == 0)
-		  msg = MISSILE_LAUNCHER_CMD_RIGHT;
-		
-		else if (strncmp(buf,up,1) == 0)
-		  msg = MISSILE_LAUNCHER_CMD_UP;
-		
-		else if (strncmp(buf,down,1) == 0)
-		  msg = MISSILE_LAUNCHER_CMD_DOWN;
-		
-		else if (strncmp(buf,fire,1) == 0)
-		  msg = MISSILE_LAUNCHER_CMD_FIRE;
-		  
-		else if (strncmp(buf,stop,1) == 0)
-		  msg = MISSILE_LAUNCHER_CMD_STOP;
+    if (strncmp(buf,left,1) == 0)
+      msg = MISSILE_LAUNCHER_CMD_LEFT;
+    
+    else if (strncmp(buf,right,1) == 0)
+      msg = MISSILE_LAUNCHER_CMD_RIGHT;
+    
+    else if (strncmp(buf,up,1) == 0)
+      msg = MISSILE_LAUNCHER_CMD_UP;
+    
+    else if (strncmp(buf,down,1) == 0)
+      msg = MISSILE_LAUNCHER_CMD_DOWN;
+    
+    else if (strncmp(buf,fire,1) == 0)
+      msg = MISSILE_LAUNCHER_CMD_FIRE;
+      
+    else if (strncmp(buf,stop,1) == 0)
+      msg = MISSILE_LAUNCHER_CMD_STOP;
 
-		
-		missile_do(control, msg, device_type);
-		usleep(300 * 1000);
-		missile_do(control, MISSILE_LAUNCHER_CMD_STOP, device_type);
+    
+    missile_do(control, msg, device_type);
+    usleep(300 * 1000);
+    missile_do(control, MISSILE_LAUNCHER_CMD_STOP, device_type);
     msg = 0x00;
    }
 
-	close(sockfd);
+  close(sockfd);
 }
 
 //=============================================================================
@@ -240,6 +240,11 @@ void *connection_handler(void *argss)
   char msg = 0x00;
   int device_type = 1;
 
+  reply_message = "Welcome to Missile Launcher Server.\n\n\rThe following commands are accepted: U, D, L, R, F, S, Q\n\r\tU - UP\n\r\tD - Down\n\r\tL - Left\n\r\tR - Right\n\r\tF - Fire\n\r\tS - Stop\n\r\tQ - Quit\n\n\rPlay nice now :)";
+  if( write(sock, reply_message, strlen(reply_message)) == -1 ){
+    printf("Error sending message\n");
+  }
+
   while( (read_size = recv(sock , client_message , 512 , 0)) > 0 )
   {
     //Send the message back to client
@@ -250,7 +255,7 @@ void *connection_handler(void *argss)
 
     printf("Received command %s from %s\n", client_message, (*client).ip);     
     
-    if (strncmp(client_message,left,strlen(left)) == 0){
+    if (strncmp(client_message, left, strlen(left)) == 0){
       msg = MISSILE_LAUNCHER_CMD_LEFT;
     
     }else if (strncmp(client_message, right, strlen(right)) == 0){
@@ -285,7 +290,7 @@ void *connection_handler(void *argss)
    
   if(read_size == 0)
   {
-    puts("Client disconnected.");
+    printf("Client %s disconnected.\n", (*client).ip);
     fflush(stdout);
   }
   else if(read_size == -1)
@@ -411,13 +416,13 @@ int main(int argc, char **argv) {
     
   default:
     printf("Device Type (%d) not implemented, please do it!\n",
-	   device_type);
+     device_type);
     return -1;
     
   }
   
   if(set_net){
-	//UDPServer();
+  //UDPServer();
   TCPServer();
   }
   
